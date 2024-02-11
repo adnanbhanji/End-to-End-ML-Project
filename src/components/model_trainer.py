4de405c132b5d1856b37876399542607964fdb51
Adnan Bhanji <bhanjiadnan786@gmail.com>
2024-02-11 19:29:51 +0100

    prediction pipeline

--- src/components/model_trainer.py
+++ src/components/model_trainer.py
@@ -50,9 +50,45 @@ class ModelTrainer:
                 "AdaBoostRegressor": AdaBoostRegressor(),
                 "XGBRegressor": XGBRegressor(),
             }
+            
+            hyperparameters={
+                "DecisionTreeRegressor": {
+                    'criterion':['squared_error', 'friedman_mse', 'absolute_error', 'poisson'],
+                    # 'splitter':['best','random'],
+                    # 'max_features':['sqrt','log2'],
+                },
+                "RandomForestRegressor":{
+                    # 'criterion':['squared_error', 'friedman_mse', 'absolute_error', 'poisson'],
+                 
+                    # 'max_features':['sqrt','log2',None],
+                    'n_estimators': [8,16,32,64,128,256]
+                },
+                "GradientBoostingRegressor":{
+                    # 'loss':['squared_error', 'huber', 'absolute_error', 'quantile'],
+                    'learning_rate':[.1,.01,.05,.001],
+                    'subsample':[0.6,0.7,0.75,0.8,0.85,0.9],
+                    # 'criterion':['squared_error', 'friedman_mse'],
+                    # 'max_features':['auto','sqrt','log2'],
+                    'n_estimators': [8,16,32,64,128,256]
+                },
+                "LinearRegression":{},
+                "XGBRegressor":{
+                    'learning_rate':[.1,.01,.05,.001],
+                    'n_estimators': [8,16,32,64,128,256]
+                },
+                "AdaBoostRegressor":{
+                    'learning_rate':[.1,.01,0.5,.001],
+                    # 'loss':['linear','square','exponential'],
+                    'n_estimators': [8,16,32,64,128,256]
+                },
+                "KNeighborsRegressor": 
+                {"n_neighbors": [3, 5, 7]},
+
+                
+            }
 
             model_report: dict = evaluate_models(
-                models, X_train, y_train, X_test, y_test
+                models, hyperparameters, X_train, y_train, X_test, y_test
             )
 
             best_model_name = max(
--- src/utils.py
+++ src/utils.py
@@ -8,8 +8,10 @@ import dill
 from pathlib import Path
 sys.path.append(str(Path(__file__).parent.parent))
 
+from logger import logging
 from exception import CustomException
 from sklearn.metrics import r2_score
+from sklearn.model_selection import GridSearchCV
 
 def save_object(obj, file_path):
     """
@@ -27,7 +29,7 @@ def save_object(obj, file_path):
     except Exception as e:
         raise CustomException("Error occurred in save_object", e)
 
-def evaluate_models(models, X_train, y_train, X_test, y_test):
+def evaluate_models(models, hyperparameters, X_train, y_train, X_test, y_test):
     """
     This function is used to evaluate the models using the training and testing data.
     :param models: models to be evaluated
@@ -38,8 +40,13 @@ def evaluate_models(models, X_train, y_train, X_test, y_test):
     :return: model_report: report of the models
     """
     try:
+        logging.info("Evaluating the models")
         model_report = {}
         for model_name, model in models.items():
+            param = hyperparameters[model_name]
+            gs = GridSearchCV(model, param, cv=5, n_jobs=-1)
+            gs.fit(X_train, y_train)
+            model.set_params(**gs.best_params_)
             model.fit(X_train, y_train) # Train the model
             y_train_pred = model.predict(X_train) # Predict the training data
             y_test_pred = model.predict(X_test) # Predict the testing data
@@ -49,8 +56,18 @@ def evaluate_models(models, X_train, y_train, X_test, y_test):
                 "train_score": train_model_score,
                 "test_score": test_model_score
             }
-            
+
         return model_report
     
     except Exception as e:
         raise CustomException("Error occurred in evaluate_models", e)
+    
+
+
+def load_object(file_path):
+    try:
+        with open(file_path, "rb") as file_obj:
+            return dill.load(file_obj)
+
+    except Exception as e:
+        raise CustomException(e, sys)
